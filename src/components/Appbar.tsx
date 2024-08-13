1a4056d036c78d5c3b7fbcd0a5eadad725da628d
abhijitsahoo99 <abhijitsahooas.01@gmail.com>
2024-08-13 11:46:28 +0530

    add: sendsol card, appbar

---
+++ src/components/Appbar.tsx
@@ -0,0 +1,12 @@
+const Appbar = () => {
+  return (
+    <>
+      <div className="flex justify-between m-6">
+        <p>sendsol.fun</p>
+        <p>connect wallet</p>
+      </div>
+      <div className="border-b-2"></div>
+    </>
+  );
+};
+export default Appbar;
